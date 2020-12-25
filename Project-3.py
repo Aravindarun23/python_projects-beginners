e567639829b1ca45cc227d09462053725679fa60
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2020-12-25 14:21:41 +0530

    Program to check odd or even

---
+++ Project-3.py
@@ -0,0 +1,9 @@
+# project-3
+# program to find the give number is odd or even
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The Given Number is Even")
+else:
+    print("The Given Number is Odd")
