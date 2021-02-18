435c24124bf9414b26e527783e6d0816fc4adc26
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:24:06 +0530

    Create 73-area-circumference-using-tuple-assignment.py

---
+++ 100 Problems/73-area-circumference-using-tuple-assignment.py
@@ -0,0 +1,14 @@
+print("START OF THE PROGRAM\n")
+
+radius = float(input("Enter radius: "))
+
+def AreaCir(radius):
+    area = 3.14 * radius * radius
+    cir = 2 * 3.14 * radius
+    return area,cir
+
+area,cir=AreaCir(radius)
+
+print("\nArea of the circle is =",area)
+print("Circumference of the circle =",cir)
+print("\nEND OF THE PROGRAM")
