5aaf7202c6e6ec0716ff4f2233c677e604aaaba3
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:59:34 +0530

    Create 77-sum-of-tuple-numbers.py

---
+++ 100 Problems/77-sum-of-tuple-numbers.py
@@ -0,0 +1,12 @@
+print("START OF THE PROGRAM\n")
+
+my_tuple = tuple(range(1,11,1))
+total = 0
+i = 0
+while i < len(my_tuple):
+    total += my_tuple[i]
+    i += 1
+
+print("My tuple:\n",my_tuple)
+print("\n Sum of My tuple=",total)
+print("\nEND OF THE PROGRAM")
