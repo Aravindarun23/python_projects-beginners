e67f14837bf9d5e9f4363cdb037859207ab0e6ac
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:15:41 +0530

    Create 72-swapping-two-values-tuple.py

---
+++ 100 Problems/72-swapping-two-values-tuple.py
@@ -0,0 +1,11 @@
+print("START OF THE PROGRAM\n")
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("\nValues before swapping","\na =",a,"\nb =",b)
+
+(a,b) = (b,a)
+
+print("\nValues After swapping","\na =",a,"\nb =",b)
+print("\nEND OF THE PROGRAM")
