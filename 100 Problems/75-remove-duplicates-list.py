bbc5a1adf94dfbf9ff30f3b79a2530a2d00542ec
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:48:34 +0530

    Create 75-remove-duplicates-list.py

---
+++ 100 Problems/75-remove-duplicates-list.py
@@ -0,0 +1,12 @@
+print("START OF THE PROGRAM\n")
+
+my_list = [1,2,3,1,5,8,3,5,5,20,10,8,3,6,2,7]
+org_list = []
+
+for i in my_list:
+    if i not in org_list:
+        org_list.append(i)
+
+print("My list:\n",my_list)
+print("\nAfter removing Duplicates:\n",org_list)
+print("\nEND OF THE PROGRAM")
