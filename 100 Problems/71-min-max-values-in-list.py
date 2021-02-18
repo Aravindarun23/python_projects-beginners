e58f263cdd90367063bcb749c3da7ca5adc3648e
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:11:31 +0530

    Create 71-min-max-values-in-list.py

---
+++ 100 Problems/71-min-max-values-in-list.py
@@ -0,0 +1,9 @@
+print("START OF THE PROGRAM\n")
+
+my_list = [10,20,30,40,50,60,70,80,90,100]
+
+print("My list:",my_list)
+print("\nMinimum value in My list:",min(my_list))
+print("Maximum value in My list:",max(my_list))
+
+print("\nEND OF THE PROGRAM")
