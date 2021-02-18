e587d08f66f846da151dd656da76bb1fd64942d1
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:52:43 +0530

    Delete 76-max-value-tuple.py

--- 76-max-value-tuple.py
+++
@@ -1,7 +0,0 @@
-print("START OF THE PROGRAM\n")
-
-my_tuple = tuple(range(1,16,1))
-
-print("My tuple:",my_tuple)
-print("\nMaximum value in My tuple =",max(my_tuple))
-print("\nEND OF THE PROGRAM")
