0455ea9f73999c6f5ceadec032cbd30269107f40
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 23:42:24 +0530

    Create seperate-positive-numbers.py

---
+++ 100 Problems/seperate-positive-numbers.py
@@ -0,0 +1,18 @@
+print("START OF THE PROGRAM\n")
+
+# generating  positive and negative numbers in a list
+my_list = list(range(-10,11,1))
+print("My list:\n",my_list)
+
+# removing negative numbers
+pos_num = []
+
+for i in my_list:
+    if i > 0:
+        pos_num.append(i)
+
+# converting list into tuple
+pos_tuple = tuple(pos_num)
+
+print("\nPositive numbers:\n",pos_tuple)
+print("\nEND OF THE PROGRAM")
