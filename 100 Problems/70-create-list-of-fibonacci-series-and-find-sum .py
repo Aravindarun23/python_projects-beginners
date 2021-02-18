62de78fc4d462a9a1ccb0d607d1053777910dc28
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 12:34:39 -0500

    Add files via upload

---
+++ 100 Problems/70-create-list-of-fibonacci-series-and-find-sum .py
@@ -0,0 +1,27 @@
+print("START OF THE PROGRAM\n")
+
+my_list = []
+n1 = 0
+n2 = 1
+count = 0
+sum = 0
+
+num = int(input("How many: "))
+
+# storing in a list
+while count < num:
+    my_list.append(n1)
+    t = n1 + n2
+    n1 = n2
+    n2 = t
+    count += 1
+    
+print("\nFibonacci series:",my_list)
+
+# adding all numbers
+for i in my_list:
+    sum += i
+
+print("\nSum of Fibonacci series is =",sum)
+print("\nEND OF THE PROGRAM")
+
