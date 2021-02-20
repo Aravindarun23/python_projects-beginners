6879092e3bac95bd5afaa3a3beace6679e43abbc
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-20 11:15:12 -0500

    Add files via upload

---
+++ 100 Problems/14-voting-eligibility.py
@@ -0,0 +1,8 @@
+# getting input
+age = int(input("Enter your age: "))
+
+# condition checking and printing
+if age >= 18:
+    print("You're eligible for voting")
+else:
+    print("You're not eligible for voting")
