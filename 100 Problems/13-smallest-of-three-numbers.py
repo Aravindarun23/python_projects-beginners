da97b81c1eb8ab720bf2460fe0ef46490cae72d8
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-20 11:12:37 -0500

    Add files via upload

---
+++ 100 Problems/13-smallest-of-three-numbers.py
@@ -0,0 +1,16 @@
+# getting input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+# condition checking and printing
+if (a > b) and (c > b):
+    print("\nSmallest number =",b)
+elif ( b > a ) and ( c > a ):
+    print("\nSmallest number =",a)
+elif ( a > c ) and ( b > c ):
+    print("\nSmallest number =",c)
+elif a == b and b == c:
+    print("\nAll numbers are Equal")
+else:
+    print("\nSome numbers are equal")
