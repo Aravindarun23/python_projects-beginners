616812f664526b52ede8b2e7df7d0119807323f3
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-20 11:26:34 -0500

    Add files via upload

---
+++ 100 Problems/15-check-positive-or-negative.py
@@ -0,0 +1,10 @@
+# getting input
+n = int(input("Enter a number: "))
+
+# condition checking and printing
+if n > 0:
+    print(n,"is a Positive number")
+elif n == 0:
+    print(n,"is Zero")
+else:
+    print(n,"is a Negative number")
