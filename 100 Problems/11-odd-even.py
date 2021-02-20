6db1d293caccf4f256c4228781fd76d06ccf65a5
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-20 11:12:05 -0500

    Add files via upload

---
+++ 100 Problems/11-odd-even.py
@@ -0,0 +1,8 @@
+# getting input
+n = int(input("Enter a number: "))
+
+# checking condition and printing output
+if n % 2 == 0:
+    print("The given number is even")
+else:
+    print("The given number is odd")
---
+++ 100 Problems/12-check-vowels.py
@@ -0,0 +1,11 @@
+# getting input
+char = input("Enter a letter: ")
+
+# vowels
+vowels = ['a','e','i','o','u']
+
+# checking condition and printing output
+if char in vowels:
+    print(char,"is a vowel")
+else:
+    print(char,"is not a vowel")
