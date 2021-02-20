5d10fa5a97c75fd2faf1c4c4074515bd4345add5
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-20 22:31:32 +0530

    Update 72-swapping-two-values-tuple.py

--- 100 Problems/72-swapping-two-values-tuple.py
+++ 100 Problems/72-swapping-two-values-tuple.py
@@ -1,5 +1,3 @@
-print("START OF THE PROGRAM\n")
-
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 
@@ -8,4 +6,3 @@ print("\nValues before swapping","\na =",a,"\nb =",b)
 (a,b) = (b,a)
 
 print("\nValues After swapping","\na =",a,"\nb =",b)
-print("\nEND OF THE PROGRAM")
