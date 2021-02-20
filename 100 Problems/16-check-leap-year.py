9fbbbd47cf5f9775404dba0cd7a987ee241a87f3
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-20 11:29:32 -0500

    Add files via upload

---
+++ 100 Problems/16-check-leap-year.py
@@ -0,0 +1,8 @@
+# getting input
+year = int(input("Enter year: "))
+
+# condition checking and printing
+if year % 4 == 0:
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")
